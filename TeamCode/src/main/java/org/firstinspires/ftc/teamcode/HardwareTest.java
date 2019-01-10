4f6bbb3a430b48136d1aeb9ad2bfd87d168ba253
Kevin Zhang <kzhang19@taboracademy.org>
2019-01-10 16:32:34 -0500

    Updated autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTest.java
@@ -1,34 +1,6 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -41,11 +13,12 @@ public class HardwareTest
     public DcMotor  rightFrontDrive  = null;
     public DcMotor  leftBackDrive   = null;
     public DcMotor  rightBackDrive  = null;
+    public DcMotor  linearSlide = null;
     //public DcMotor  leftArm     = null;
-    //public Servo arm    = null;
+    public CRServo    grabServo    = null;
+    public DcMotor    armMotor   = null;
 
-
-    public static final double ARM_HOME       =  0.0 ;
+    public static final double MID_SERVO       =  0.5 ;
     public static final double ARM_UP_POWER    =  0.45 ;
     public static final double ARM_DOWN_POWER  = -0.45 ;
 
@@ -68,7 +41,11 @@ public class HardwareTest
         leftBackDrive  = hwMap.get(DcMotor.class, "Motor 3");
         rightFrontDrive = hwMap.get(DcMotor.class, "Motor 2");
         rightBackDrive = hwMap.get(DcMotor.class, "Motor 4");
-        //leftArm    = hwMap.get(DcMotor.class, "left_arm");
+        linearSlide    = hwMap.get(DcMotor.class, "Extend");
+
+        grabServo = hwMap.get(CRServo.class, "Grab");
+        armMotor = hwMap.get(DcMotor.class, "Arm");
+
         leftFrontDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         leftBackDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
@@ -89,9 +66,9 @@ public class HardwareTest
         //leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Define and initialize ALL installed servos.
-        //arm  = hwMap.get(Servo.class, "arm");
-        //arm.setPosition(ARM_HOME);
-
+        //leftClaw  = hwMap.get(Servo.class, "left_hand");
+        //rightClaw = hwMap.get(Servo.class, "right_hand");
+        //leftClaw.setPosition(MID_SERVO);
+        //rightClaw.setPosition(MID_SERVO);
     }
- }
-
+}
