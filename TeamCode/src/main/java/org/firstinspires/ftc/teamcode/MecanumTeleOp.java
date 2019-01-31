d5ff68bf55b1a2b80ddc3e6ef043bac743764da6
jakecarlson <flyingchipmunk.kfc@gmail.com>
2019-01-31 15:58:33 -0500

    Merge branch 'master' of https://github.com/taborrobotics12667/ftc_app
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.teamcode.MecanumDriveTrain;
+import org.firstinspires.ftc.teamcode.DriveTrain;
 
 import java.util.ArrayList;
 
@@ -17,81 +17,39 @@ public class MecanumTeleOp extends LinearOpMode {
 
     private double maxPower = maxPower1 + 0.2 * conditional;
 
-    private MecanumDriveTrain robot;
-
+    HardwareTest ht = new HardwareTest();
 
     @Override
     public void runOpMode() {
-
-        robot = new MecanumDriveTrain(hardwareMap, "Motor 1", "Motor 2", "Motor 3", "Motor 4","Lift");
 
         waitForStart();
 
         while (opModeIsActive()) {
             double Rpower = Range.clip(gamepad1.left_stick_y + gamepad1.left_stick_x, -maxPower, maxPower);
             double Lpower = Range.clip(gamepad1.left_stick_y - gamepad1.left_stick_x, -maxPower, maxPower);
-            double diagonalY = Range.clip(gamepad1.right_stick_y, -maxPower, maxPower);
-            double diagonalX = Range.clip(gamepad1.right_stick_x, -maxPower, maxPower);
+
+
 
             double liftPower = Range.clip(gamepad1.left_trigger - gamepad1.right_trigger, -0.75, 0.75);
 
 
-            if (gamepad1.right_bumper){
-                robot.slide(0.7);
-            }
-            else if (gamepad1.left_bumper){
-                robot.slide(-0.7);
-            }
 
 
-
-            if (diagonalX != 0 && diagonalY != 0) {
-                if (diagonalY > 0) {
-                    if (diagonalX < 0) {
-                        robot.diagonalTL(maxPower1, -1);
-                    }
-                    else if (diagonalX > 0){
-                        robot.diagonalTR(maxPower1, -1);
-                    }
-                }
-                else if (diagonalY < 0) {
-
-                    if (diagonalX < 0){
-                        robot.diagonalTR(maxPower1, 1);
-                    }
-                    else if (diagonalX > 0) {
-                        robot.diagonalTL(maxPower1, 1);
-                        String yes = "YES";
-                        telemetry.addData("IF?", yes);
-                        telemetry.update();
-                    }
-
-                }
+            ht.leftDrive.setPower(Lpower);
+            ht.rightDrive.setPower(Rpower);
 
 
-            }
-            else {
+            String messge = "Right Power: " + Double.toString(Rpower) + "Left Power: " + Double.toString(Lpower);
 
-                robot.setPower(-Rpower, -Lpower);
+            telemetry.addData("Info:", messge);
 
-                String messge = "Right Power: " + Double.toString(Rpower) + "Left Power: " + Double.toString(Lpower);
+            telemetry.update();
 
-                telemetry.addData("Info:", messge);
+            conditional = gamepad1.left_stick_button ? 1 : 0;
+            maxPower = maxPower1 + conditional * 0.2;
 
-                telemetry.update();
+            ht.lift.setPower(liftPower);
 
-                conditional = gamepad1.left_stick_button ? 1 : 0;
-                maxPower = maxPower1 + conditional * 0.2;
-
-            }
-
-
-
-            robot.Lift(liftPower);
-
-
-            String diags = "X: " + Double.toString(diagonalX) + " Y: " + Double.toString(diagonalY);
-            telemetry.addData("Diagonals: ", diags);
             telemetry.update();
         }
     }
