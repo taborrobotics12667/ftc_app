c06dd22ba879f8546f123980a5a3de2d301e58eb
jakecarlson <flyingchipmunk.kfc@gmail.com>
2019-01-31 17:39:07 -0500

    Merge branch 'master' of https://github.com/taborrobotics12667/ftc_app
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -10,7 +10,7 @@ public class MecanumTeleOp extends LinearOpMode {
     private double maxPower1 = 0.5;
     double rotate;
     boolean grab = true;
-    boolean FlipBox = true;
+    double FlipBox = 1;
     double armPos = 0.0;
     private int conditional = 1;
 
@@ -29,65 +29,58 @@ public class MecanumTeleOp extends LinearOpMode {
             double liftPower = Range.clip(gamepad1.left_trigger - gamepad1.right_trigger, -0.75, 0.75);
             double extendPower = Range.clip(gamepad1.right_stick_y, -0.75, 0.75);
 
-            if (gamepad1.right_bumper){
+            if (gamepad1.right_bumper) {
                 ht.armFlip.setPower(-maxPower);
-            }
-            else if (gamepad1.left_bumper){
+            } else if (gamepad1.left_bumper) {
                 ht.armFlip.setPower(maxPower);
-            }
-
-            else {
+            } else {
                 ht.armFlip.setPower(0);
             }
-            if (gamepad1.a){
-                if(grab){
+            if (gamepad1.a) {
+                if (grab) {
                     ht.grabServo.setPower(.75);
-                }
-                else {
+                } else {
                     ht.grabServo.setPower(0);
 
                 }
                 grab = !grab;
             }
-            if (gamepad1.a) {
-                armPos = 0.0;
+            if (gamepad1.b) {
+                if (FlipBox == 0) {
+                    armPos = 0.0;
+                } else if (FlipBox == 1) {
+                    armPos = 0.25;
+                } else if (FlipBox == 2) {
+                    armPos = 0.75;
+                }
+                FlipBox = (FlipBox + 1) % 3;
+
+
+                armPos = Range.clip(armPos, 0.0, 1.0);
+                ht.boxFlip.setPosition(armPos);
+
+                telemetry.addData("arm", "%.2f", armPos);
+
+                telemetry.update();
+
+                ht.leftDrive.setPower(Lpower);
+                ht.rightDrive.setPower(Rpower);
+                ht.armExtend.setPower(extendPower);
+                ht.lift.setPower(liftPower);
+
+
+                String messge = "Right Power: " + Double.toString(Rpower) + "Left Power: " + Double.toString(Lpower);
+
+                telemetry.addData("Info:", messge);
+
+                telemetry.update();
+
+                conditional = gamepad1.left_stick_button ? 1 : 0;
+                maxPower = maxPower1 + conditional * 0.2;
+
+
+                telemetry.update();
             }
-            else if (gamepad1.b) {
-                armPos = 0.25;
-            }
-            else if (gamepad1.y){
-                armPos = 0.75;
-            }
-
-
-            armPos = Range.clip(armPos, 0.0, 1.0);
-            ht.boxFlip.setPosition(armPos);
-
-            telemetry.addData("arm", "%.2f", armPos);
-
-            telemetry.update();
-        }
-
-
-
-
-
-            ht.leftDrive.setPower(Lpower);
-            ht.rightDrive.setPower(Rpower);
-
-
-            String messge = "Right Power: " + Double.toString(Rpower) + "Left Power: " + Double.toString(Lpower);
-
-            telemetry.addData("Info:", messge);
-
-            telemetry.update();
-
-            conditional = gamepad1.left_stick_button ? 1 : 0;
-            maxPower = maxPower1 + conditional * 0.2;
-
-            ht.lift.setPower(liftPower);
-
-            telemetry.update();
         }
     }
 }
