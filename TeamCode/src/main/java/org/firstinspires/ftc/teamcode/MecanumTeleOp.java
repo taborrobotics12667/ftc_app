bea6b2a0f25e82d974f35210f723587ab6e7cd78
jakecarlson <flyingchipmunk.kfc@gmail.com>
2019-01-31 16:31:06 -0500

    Merge branch 'master' of https://github.com/taborrobotics12667/ftc_app
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -4,15 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.teamcode.DriveTrain;
-
-import java.util.ArrayList;
-
 @TeleOp
 public class MecanumTeleOp extends LinearOpMode {
 
     private double maxPower1 = 0.5;
-
+    double rotate;
+    boolean grab = true;
+    boolean FlipBox = true;
+    double armPos = 0.0;
     private int conditional = 1;
 
     private double maxPower = maxPower1 + 0.2 * conditional;
@@ -27,10 +26,48 @@ public class MecanumTeleOp extends LinearOpMode {
         while (opModeIsActive()) {
             double Rpower = Range.clip(gamepad1.left_stick_y + gamepad1.left_stick_x, -maxPower, maxPower);
             double Lpower = Range.clip(gamepad1.left_stick_y - gamepad1.left_stick_x, -maxPower, maxPower);
-
-
-
             double liftPower = Range.clip(gamepad1.left_trigger - gamepad1.right_trigger, -0.75, 0.75);
+            double extendPower = Range.clip(gamepad1.right_stick_y, -0.75, 0.75);
+
+            if (gamepad1.right_bumper){
+                ht.armFlip.setPower(-maxPower);
+            }
+            else if (gamepad1.left_bumper){
+                ht.armFlip.setPower(maxPower);
+            }
+
+            else {
+                ht.armFlip.setPower(0);
+            }
+            if (gamepad1.a){
+                if(grab){
+                    ht.grabServo.setPower(.75);
+                }
+                else {
+                    ht.grabServo.setPower(0);
+
+                }
+                grab = !grab;
+            }
+            if (gamepad1.a) {
+                armPos = 0.0;
+            }
+            else if (gamepad1.b) {
+                armPos = 0.25;
+            }
+            else if (gamepad1.y){
+                armPos = 0.75;
+            }
+
+
+            armPos = Range.clip(armPos, 0.0, 1.0);
+            ht.boxFlip.setPosition(armPos);
+
+            telemetry.addData("arm", "%.2f", armPos);
+
+            telemetry.update();
+        }
+
 
 
 
